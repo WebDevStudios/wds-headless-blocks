62737e320ab2aa9839fdc6659230d4905b150cc9
R A Van Epps <rave@ravanepps.com>
2021-10-06 15:16:25 -0600

    Split into 2 filters

--- js/blocks.js
+++ js/blocks.js
@@ -6,8 +6,9 @@
  */
 
 const {validateThemeColors, validateThemeGradients} = wp.blockEditor
-const {useEffect} = wp.element
+const {useEffect, createElement} = wp.element
 const {addFilter, applyFilters} = wp.hooks
+const {createHigherOrderComponent} = wp.compose
 
 /**
  * Customize core block settings.
@@ -25,14 +26,8 @@ wp.domReady(() => {
   })
 })
 
-addFilter(
-  'blocks.registerBlockType',
-  'wds/filterBlockColorAttrs',
-  wdsAddColorPaletteHexValues
-)
-
 /**
- * Filter block registration to add custom color attributes to specified blocks.
+ * Filter block registration to add custom color attributes.
  *
  * @author WebDevStudios
  * @since 1.0.0
@@ -40,9 +35,11 @@ addFilter(
  * @return {object}          Block settings config.
  */
 function wdsAddColorPaletteHexValues(settings) {
+  const attributes = {}
+
   // Add background color hex attribute.
   if (settings.attributes.hasOwnProperty('backgroundColor')) {
-    settings.attributes.backgroundColorHex = {
+    attributes.backgroundColorHex = {
       type: 'string',
       default: ''
     }
@@ -50,7 +47,7 @@ function wdsAddColorPaletteHexValues(settings) {
 
   // Add gradient background hex attribute.
   if (settings.attributes.hasOwnProperty('gradient')) {
-    settings.attributes.gradientHex = {
+    attributes.gradientHex = {
       type: 'string',
       default: ''
     }
@@ -58,7 +55,7 @@ function wdsAddColorPaletteHexValues(settings) {
 
   // Add main color hex attribute.
   if (settings.attributes.hasOwnProperty('mainColor')) {
-    settings.attributes.mainColorHex = {
+    attributes.mainColorHex = {
       type: 'string',
       default: ''
     }
@@ -66,7 +63,7 @@ function wdsAddColorPaletteHexValues(settings) {
 
   // Add overlay color hex attribute.
   if (settings.attributes.hasOwnProperty('overlayColor')) {
-    settings.attributes.overlayColorHex = {
+    attributes.overlayColorHex = {
       type: 'string',
       default: ''
     }
@@ -74,100 +71,121 @@ function wdsAddColorPaletteHexValues(settings) {
 
   // Add text color hex attribute.
   if (settings.attributes.hasOwnProperty('textColor')) {
-    settings.attributes.textColorHex = {
+    attributes.textColorHex = {
       type: 'string',
       default: ''
     }
   }
 
-  return {
-    ...settings,
-    edit(props) {
-      const {
-        attributes: {
-          backgroundColor,
-          gradient,
-          mainColor,
-          overlayColor,
-          textColor
-        }
-      } = props
-
-      useEffect(() => {
-        // Note: This may not work as expected if a custom theme palette has been set.
-        // In that case, this filter may need to be customized.
-        const defaultColors = validateThemeColors()
-
-        const defaultGradients = validateThemeGradients()
-
-        // Check for presence of background color attr.
-        if (backgroundColor) {
-          // Get color object by slug.
-          const backgroundColorObj = defaultColors.filter(
-            (color) => color.slug === backgroundColor
-          )
-
-          // Retrieve color hex value.
-          props.attributes.backgroundColorHex =
-            backgroundColorObj?.[0]?.color || null
-        } else {
-          delete props.attributes.backgroundColorHex
-        }
-
-        // Check for presence of gradient color attr.
-        if (gradient) {
-          // Get color object by slug.
-          const gradientObj = defaultGradients.filter(
-            (color) => color.slug === gradient
-          )
-
-          // Retrieve color hex value.
-          props.attributes.gradientHex = gradientObj?.[0]?.gradient || null
-        } else {
-          delete props.attributes.gradientHex
-        }
-
-        // Check for presence of main color attr.
-        if (mainColor) {
-          // Get color object by slug.
-          const mainColorObj = defaultColors.filter(
-            (color) => color.slug === mainColor
-          )
-
-          // Retrieve color hex value.
-          props.attributes.mainColorHex = mainColorObj?.[0]?.color || null
-        } else {
-          delete props.attributes.mainColorHex
-        }
-
-        // Check for presence of overlay color attr.
-        if (overlayColor) {
-          // Get color object by slug.
-          const overlayColorObj = defaultColors.filter(
-            (color) => color.slug === overlayColor
-          )
-
-          // Retrieve color hex value.
-          props.attributes.overlayColorHex = overlayColorObj?.[0]?.color || null
-        } else {
-          delete props.attributes.overlayColorHex
-        }
-
-        // Check for presence of text color attr.
-        if (textColor) {
-          // Get color object by slug.
-          const textColorObj = defaultColors.filter(
-            (color) => color.slug === textColor
-          )
-
-          // Retrieve color hex value.
-          props.attributes.textColorHex = textColorObj?.[0]?.color || null
-        } else {
-          delete props.attributes.textColorHex
-        }
-      }, [backgroundColor, mainColor, textColor])
-
-      return settings.edit(props)
-    }
-  }
+  return lodash.assign({}, settings, {
+    attributes: lodash.assign({}, settings.attributes, attributes)
+  })
 }
+
+addFilter(
+  'blocks.registerBlockType',
+  'wds/filterBlockColorAttrs',
+  wdsAddColorPaletteHexValues
+)
+
+/**
+ * Filter block edit function to set custom color attributes.
+ *
+ * @author WebDevStudios
+ * @since 1.0.1
+ */
+const withColorPaletteHexValues = createHigherOrderComponent((BlockEdit) => {
+  return (props) => {
+    const {
+      attributes: {
+        backgroundColor,
+        gradient,
+        mainColor,
+        overlayColor,
+        textColor
+      }
+    } = props
+
+    useEffect(() => {
+      // Note: This may not work as expected if a custom theme palette has been set.
+      // In that case, this filter may need to be customized.
+      const defaultColors = validateThemeColors()
+
+      const defaultGradients = validateThemeGradients()
+
+      // Check for presence of background color attr.
+      if (backgroundColor) {
+        // Get color object by slug.
+        const backgroundColorObj = defaultColors.filter(
+          (color) => color.slug === backgroundColor
+        )
+
+        // Retrieve color hex value.
+        props.attributes.backgroundColorHex =
+          backgroundColorObj?.[0]?.color || null
+      } else {
+        delete props.attributes.backgroundColorHex
+      }
+
+      // Check for presence of gradient color attr.
+      if (gradient) {
+        // Get color object by slug.
+        const gradientObj = defaultGradients.filter(
+          (color) => color.slug === gradient
+        )
+
+        // Retrieve color hex value.
+        props.attributes.gradientHex = gradientObj?.[0]?.gradient || null
+      } else {
+        delete props.attributes.gradientHex
+      }
+
+      // Check for presence of main color attr.
+      if (mainColor) {
+        // Get color object by slug.
+        const mainColorObj = defaultColors.filter(
+          (color) => color.slug === mainColor
+        )
+
+        // Retrieve color hex value.
+        props.attributes.mainColorHex = mainColorObj?.[0]?.color || null
+      } else {
+        delete props.attributes.mainColorHex
+      }
+
+      // Check for presence of overlay color attr.
+      if (overlayColor) {
+        // Get color object by slug.
+        const overlayColorObj = defaultColors.filter(
+          (color) => color.slug === overlayColor
+        )
+
+        // Retrieve color hex value.
+        props.attributes.overlayColorHex = overlayColorObj?.[0]?.color || null
+      } else {
+        delete props.attributes.overlayColorHex
+      }
+
+      // Check for presence of text color attr.
+      if (textColor) {
+        // Get color object by slug.
+        const textColorObj = defaultColors.filter(
+          (color) => color.slug === textColor
+        )
+
+        // Retrieve color hex value.
+        props.attributes.textColorHex = textColorObj?.[0]?.color || null
+      } else {
+        delete props.attributes.textColorHex
+      }
+    }, [backgroundColor, mainColor, textColor])
+
+    return createElement(BlockEdit, props)
+  }
+}, 'withColorPaletteHexValues')
+
+addFilter(
+  'editor.BlockEdit',
+  'wds/filterBlockEditColorAttrs',
+  withColorPaletteHexValues
+)
